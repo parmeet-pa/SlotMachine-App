ee2239e1395a409ae4d5ee84cb5b5c36016dbe1e
parmeet-pa <parmeet.ssaini@gmail.com>
2024-12-23 08:47:45 +0530

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Parmeet Singh on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
